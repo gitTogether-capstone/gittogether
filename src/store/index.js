c09e97a83448b1af1cc64b14e16d8f5702845f4f
Matt Yard <mattyard11@gmail.com>
2022-03-18 12:45:42 -0400

    adds projects reducer to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import user from './user';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import user from "./user";
+import projects from "./projects";
 
 const reducer = combineReducers({
   user,
+  projects,
 });
 
 const store = createStore(reducer);
