4b0e2216f27051f197e5050d1a5c0984e6c100ea
Greg Lee <greglee0921@gmail.com>
2022-03-18 19:31:15 -0400

    Navbar styling and format

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,25 +1,44 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../../store/user";
 import { Link } from "react-router-dom";
+import supabase from "../../client";
+import "./navbar.scss";
+import AddIcon from "@mui/icons-material/Add";
+import Login from "../Login";
 
-export default function Navbar() {
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  const checkUser = () => {
+    const user = supabase.auth.user();
+    dispatch(setUser(user));
+  };
+
   return (
     <div id="navBar">
+      <div className="wrapper">
       <div id="leftNav">
-        <span>gitTogether</span>
-        <div>
+        <Link to="/" className="logo">gitTogether</Link>
+        <div className="itemContainer">
           <span>Browse Ideas</span>
         </div>
-        <div>
+        <div className="itemContainer">
           <span>Messages</span>
         </div>
       </div>
       <div id="rightNav">
-        <div className="iconContainer">
-          <span></span>
+        <div className="itemContainer">
+          <AddIcon className="icon" />
         </div>
-        <div className="loginButton">Login</div>
+        <div className="loginButton">
+          <Link to="/login">Login</Link>
+        </div>
+      </div>
       </div>
     </div>
   );
-}
+};
+
+export default Navbar;
