48b30b215174a58763e3f455e31738a59358646f
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-03-19 14:39:14 -0700

    user profile displays languages

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -6,7 +6,30 @@ import './style.css';
 function UserProfile(props) {
   const [loading, setLoading] = useState(true);
   const userStore = useSelector((state) => state.user);
-  const [languages, setLanguages] = useState(null);
+  const [languages, setLanguages] = useState([]);
+
+  useEffect(() => {
+    let user = supabase.auth.user();
+    async function fetchLanguages() {
+      let { data, error } = await supabase
+        .from('userLanguages')
+        .select(
+          `
+        languageId, userId,
+        languages (name)
+        `
+        )
+        .eq('userId', user.id);
+      let fetchedlanguages = [];
+      for (let i = 0; i < data.length; i++) {
+        fetchedlanguages.push(data[i].languages.name);
+      }
+      setLanguages(fetchedlanguages);
+    }
+    fetchLanguages();
+  }, []);
+
+  console.log(`languages`, languages);
 
   return (
     <div id="user-profile">
@@ -16,7 +39,19 @@ function UserProfile(props) {
           src={userStore.identities[0]['identity_data'].avatar_url}
         />
       </div>
-      <div id="user-bio-languages"></div>
+      <div id="user-bio-languages">
+        <div id="user-languages">
+          {languages.length > 0
+            ? languages.map((language, i) => {
+                return (
+                  <div key={i} id="language">
+                    {language}
+                  </div>
+                );
+              })
+            : null}
+        </div>
+      </div>
       <div id="user-projects"></div>
     </div>
   );
