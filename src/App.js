b09ae02ed9d099c12f7ee3771548ff6a75efe8b8
Greg Lee <greglee0921@gmail.com>
2022-03-18 20:14:53 -0400

    Navbar fix

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Routes from "./Routes";
+import Navbar from "./components/navbar/Navbar";
 import supabase from "./client";
 import { setUser } from "./store/user";
+
 
 function App() {
   const dispatch = useDispatch();
@@ -21,6 +23,7 @@ function App() {
   };
   return (
     <div className="App">
+      <Navbar />
       <Routes />
     </div>
   );
