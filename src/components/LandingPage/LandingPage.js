9167f420999c0dcf4f3db74484cfb71810f8f442
Greg Lee <greglee0921@gmail.com>
2022-03-21 18:56:39 -0400

    Replaced Home with Landing Page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import  "./LandingPage.scss"
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page Placeholder</h1>
-      <Link to="/login">Login</Link>
     </div>
   );
 };
