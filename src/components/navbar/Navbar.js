89820f4fb9990434b38c2a4b1431a254a15fc19f
Greg Lee <greglee0921@gmail.com>
2022-03-22 11:41:10 -0400

    Navbar user links only show when logged in

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setUser, signOut } from '../../store/user';
-import { Link } from 'react-router-dom';
-import supabase from '../../client';
-import './navbar.scss';
-import AddIcon from '@mui/icons-material/Add';
-import Login from '../Login';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser, signOut } from "../../store/user";
+import { Link } from "react-router-dom";
+import supabase from "../../client";
+import "./navbar.scss";
+import AddIcon from "@mui/icons-material/Add";
+import Login from "../Login";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-
-  const checkUser = () => {
-    const user = supabase.auth.user();
-    dispatch(setUser(user));
-  };
 
   const logout = () => {
     dispatch(signOut());
@@ -27,24 +22,20 @@ const Navbar = () => {
           <Link to="/" className="logo">
             gitTogether
           </Link>
-          <div className="itemContainer">
-            <span>Browse Ideas</span>
-          </div>
-          <div className="itemContainer">
-            <span>Messages</span>
-          </div>
         </div>
-        <div id="rightNav">
-          <div className="itemContainer">
-            <Link to="/addProject">
-              <AddIcon className="icon" />
-            </Link>
-          </div>
-          <div>
-            {user && user.id ? (
+          <div id="rightNav">
+            {user?.id ? (
               <>
+                <div className="itemContainer">
+                  <span>Messages</span>
+                </div>
+                <div className="itemContainer">
+                  <Link to="/addProject">
+                    <AddIcon className="icon" />
+                  </Link>
+                </div>
                 <Link
-                  to={`/user/${user.identities[0]['identity_data'].user_name}`}
+                  to={`/user/${user.identities[0]["identity_data"].user_name}`}
                   className="profilePic"
                 >
                   <img src={user.user_metadata.avatar_url} alt="profile" />
@@ -59,7 +50,6 @@ const Navbar = () => {
               </Link>
             )}
           </div>
-        </div>
       </div>
     </div>
   );
