eef2326b007618d3ad28d7675e0a706ef980d6de
Greg Lee <greglee0921@gmail.com>
2022-03-23 13:01:12 -0400

    main footer content done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,35 @@
 import React from "react";
 import "./footer.scss";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import CopyrightIcon from "@mui/icons-material/Copyright";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer">
         <div className="footer-heading footer-1">
-          <h2>Company</h2>
+          <h4>Why gitTogether?</h4>
           <ul>
-            <li>About Us</li>
-            <li>Our Services</li>
-            <li>Privacy Policy</li>
-            <li>Affiliate Program</li>
+            <li>Our Mission</li>
+            <li>Engagement</li>
+            <li>Scale</li>
+            <li>Watch Demo</li>
           </ul>
         </div>
         <div className="footer-heading footer-2">
-          <h2>Company</h2>
+          <h4>Product</h4>
           <ul>
-            <li>About Us</li>
-            <li>Our Services</li>
-            <li>Privacy Policy</li>
-            <li>Affiliate Program</li>
+            <li>Features</li>
+            <li>Integrations</li>
+            <li>Enteprise</li>
+            <li>Solutions</li>
           </ul>
         </div>
         <div className="footer-heading footer-3">
-          <h2>Company</h2>
+          <h4>Company</h4>
           <ul>
             <li>About Us</li>
             <li>Our Services</li>
@@ -32,6 +37,28 @@ const Footer = () => {
             <li>Affiliate Program</li>
           </ul>
         </div>
+        <div className="footer-heading footer-4">
+          <h4>Follow Us</h4>
+          <div className="socials">
+            <TwitterIcon />
+            <YouTubeIcon />
+            <FacebookIcon />
+            <LinkedInIcon />
+          </div>
+        </div>
+      </div>
+      <div className="legal">
+        <span>
+          <CopyrightIcon />
+          gitTogether
+        </span>
+        <span>gitTogether</span>
+        <span>Legal</span>
+        <span>Privacy Center</span>
+        <span>Privacy Policy</span>
+        <span>Cookies</span>
+        <span>About ads</span>
+        <span>Additional Privacy Disclosures</span>
       </div>
     </div>
   );
