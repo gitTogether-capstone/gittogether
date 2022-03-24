0fc0ea3fee6eef436ed0a911f03f28dcd49be1f8
Fiora De Marco <fiorademarco7@gmail.com>
2022-03-24 14:10:16 -0400

    added styling to create project made it a popup, fetched all project users

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setUser, signOut } from '../../store/user';
-import { Link } from 'react-router-dom';
-import supabase from '../../client';
-import './navbar.scss';
-import AddIcon from '@mui/icons-material/Add';
-import NotificationsIcon from '@mui/icons-material/NotificationsOutlined';
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser, signOut } from "../../store/user";
+import { Link } from "react-router-dom";
+import supabase from "../../client";
+import "./navbar.scss";
+import AddIcon from "@mui/icons-material/Add";
+import NotificationsIcon from "@mui/icons-material/NotificationsOutlined";
+import Popup from "../AddProject/Popup";
+import { useState } from "react";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-
+  const [buttonPopup, setButtonPopup] = useState(false);
   const logout = () => {
     dispatch(signOut());
   };
 
   return (
-    <div className="navBar">
-      <div className="leftNav">
-        <Link to="/" className="logo">
+    <div className='navBar'>
+      <div className='leftNav'>
+        <Link to='/' className='logo'>
           gitTogether
         </Link>
       </div>
       {user?.id ? (
-        <div className="rightNav">
-          <div className="itemContainer">
+        <div className='rightNav'>
+          <div className='itemContainer'>
             <span>Messages</span>
           </div>
-          <div className="itemContainer">
-            <Link to="/addProject">
-              <AddIcon className="icon" />
-            </Link>
+          <div className='itemContainer'>
+            <button onClick={() => setButtonPopup(true)}>+</button>
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup>
           </div>
-          <div className="itemContainer">
+          <div className='itemContainer'>
             <NotificationsIcon sx={{ fontSize: 30 }} />
           </div>
-          <div className="img-div">
-            <Link to={`/user/${user.identities[0]['identity_data'].user_name}`}>
+          <div className='img-div'>
+            <Link to={`/user/${user.identities[0]["identity_data"].user_name}`}>
               <img
-                className="profilePic"
+                className='profilePic'
                 src={user.user_metadata.avatar_url}
-                alt="profile"
+                alt='profile'
               />
-            </Link>{' '}
+            </Link>{" "}
           </div>
-          <div className="button-div">
-            <button className="logButton" onClick={logout}>
+          <div className='button-div'>
+            <button className='logButton' onClick={logout}>
               Logout
             </button>
           </div>
         </div>
       ) : (
-        <Link to="/login">
-          <button className="logButton">Login</button>
+        <Link to='/login'>
+          <button className='logButton'>Login</button>
         </Link>
       )}
     </div>
