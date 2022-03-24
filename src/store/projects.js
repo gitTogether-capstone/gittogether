66e053b05f2915567a0c6ff19c2e75b5ff0947f5
Matt Yard <mattyard11@gmail.com>
2022-03-24 15:12:22 -0400

    bug fixes on project feed

--- src/store/projects.js
+++ src/store/projects.js
@@ -1,9 +1,11 @@
 import supabase from '../client';
 const SET_PROJECTS = 'SET_PROJECTS';
 const ADD_PROJECTS = 'ADD_PROJECTS';
+export const END_PROJECTS = 'END_PROJECTS';
 
 export const setProjects = (projects) => ({ type: SET_PROJECTS, projects });
 const addProjects = (projects) => ({ type: ADD_PROJECTS, projects });
+const endProjects = () => ({ type: END_PROJECTS });
 
 export const fetchProjects = (filters, categories, languages, page, type) => {
   return async (dispatch) => {
@@ -37,11 +39,14 @@ export const fetchProjects = (filters, categories, languages, page, type) => {
     if (error) {
       console.log(error);
     }
-
-    if (type === 'initial') {
-      dispatch(setProjects(projects));
-    } else if (type === 'more') {
-      dispatch(addProjects(projects));
+    if (projects.length === 0) {
+      dispatch(endProjects());
+    } else {
+      if (type === 'initial') {
+        dispatch(setProjects(projects));
+      } else if (type === 'more') {
+        dispatch(addProjects(projects));
+      }
     }
   };
 };
