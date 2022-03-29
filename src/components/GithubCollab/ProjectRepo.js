84cc10ca4bdf2a2e5c425488197d082aa9f2f4d7
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-03-29 10:04:56 -0700

    users can add a repo to an already existing project if they own it

--- src/components/GithubCollab/ProjectRepo.js
+++ src/components/GithubCollab/ProjectRepo.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import supabase from '../../client';
+import { Octokit } from '@octokit/core';
+import { useDispatch } from 'react-redux';
+import { updateRepo } from '../../store/project';
 
 function ProjectRepo(props) {
   const user = supabase.auth.user();
+  const userSession = supabase.auth.session();
+  const octokit = new Octokit({
+    auth: userSession.provider_token,
+  });
+  const [repoName, setRepoName] = useState('');
+  const dispatch = useDispatch();
   console.log(props);
   console.log(user);
+
+  const verifyRepo = async (evt) => {
+    try {
+      let repository = repoName.split('/');
+      let newreponame = repository[repository.length - 1];
+      await octokit.request(`GET /repos/{owner}/{repo}`, {
+        owner: userSession.user.user_metadata.user_name,
+        repo: newreponame,
+      });
+      dispatch(updateRepo(newreponame));
+      await supabase
+        .from('projects')
+        .update({ repoLink: evt.target.value })
+        .eq('id', props.project.id);
+    } catch (err) {
+      alert('You can not provide a repository you are not the owner of.');
+    }
+  };
+
   if (props.project.repoLink) {
     return (
       <a
@@ -22,13 +50,36 @@ function ProjectRepo(props) {
     user.id === props.project.projectUser[0].user.id
   ) {
     return (
-      <button
-        className="create-repo-button"
-        style={{ fontSize: '17px' }}
-        onClick={(e) => props.setShowRepoCreation(true)}
+      <div
+        id="no-repo"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
       >
-        Create Repo
-      </button>
+        <button
+          className="create-repo-button"
+          style={{ fontSize: '17px', width: 'fit-content' }}
+          onClick={(e) => props.setShowRepoCreation(true)}
+        >
+          Create Repo
+        </button>
+        <div>
+          <input
+            name="name"
+            type="text"
+            className="repo-form-input"
+            required
+            maxLength={100}
+            onChange={(e) => setRepoName(e.target.value)}
+          ></input>
+          <button className="create-repo-button" onClick={(e) => verifyRepo(e)}>
+            Add Repo
+          </button>
+        </div>
+      </div>
     );
   } else {
     return <div>This project has no repo yet.</div>;
