6ba23c368d7ce6a199ba74b2afbb0283d973813b
Matt Yard <mattyard11@gmail.com>
2022-03-29 19:39:21 -0400

    adds utility function to fetch all user dms

--- src/util.js
+++ src/util.js
@@ -41,3 +41,30 @@ export const fetchProjectRequests = async (userId) => {
     .eq('isAccepted', false);
   return data;
 };
+
+export const fetchUserDMs = async (currentUserId) => {
+  const { data } = await supabase
+    .from('directMessages')
+    .select(
+      `
+  sender:user!directMessages_sender_Id_fkey(id, username, imageUrl),
+  receiver: user!directMessages_receiver_Id_fkey(id, username, imageUrl)
+  `
+    )
+    .or(`receiver_Id.eq.${currentUserId}, sender_Id.eq.${currentUserId}`);
+
+  let seen = {};
+  let users = [];
+
+  for (const element of data) {
+    if (element.receiver.id !== currentUserId && !seen[element.receiver.id]) {
+      users.push(element.receiver);
+      seen[element.receiver.id] = true;
+    }
+    if (element.sender.id !== currentUserId && !seen[element.sender.id]) {
+      users.push(element.sender);
+      seen[element.sender.id] = true;
+    }
+  }
+  return users;
+};
