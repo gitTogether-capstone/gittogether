94e5059931b6795900d9225c8c7ada44697bd58e
Greg Lee <greglee0921@gmail.com>
2022-04-03 02:47:21 -0400

    style intro

--- src/components/LandingPage/Intro/Intro.js
+++ src/components/LandingPage/Intro/Intro.js
@@ -1,10 +1,33 @@
 import React from "react";
-import  "./intro.scss";
+import "./intro.scss";
 
 const Intro = () => {
   return (
     <div className="intro">
-      <h1>Ready, Set, gitTogether!</h1>
+      <div className="intro-text">
+        <div className="intro-header">
+          <h1>Where the world gits together</h1>
+        </div>
+        <div className="intro-description">
+          <h2>
+            Millions of developers are colloborating together on gitTogether-the
+            largest and most advanced software development social platform in
+            the world.
+          </h2>
+        </div>
+        <div className="intro-signup-button">
+          <button>Sign up for gitTogether</button>
+        </div>
+      </div>
+
+      <div className="world-container">
+        <img className="world-img" src="assets/gitHub_world.png" alt="" />
+        <img
+          className="astro-img"
+          src="assets/cute-astronaut-floating-space-no-stars.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
